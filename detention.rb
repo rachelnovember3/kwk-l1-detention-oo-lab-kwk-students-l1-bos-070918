910a2faa6ebf582eed264abb2f512f1aaa76fc93
rachelnovember3 <rachelnovember3@gmail.com>
2018-07-16 13:30:40 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,1 +1,11 @@
 #Code your detention class here
+class Detention
+  attr_accessor :location, :time, :grumpy_teacher
+  
+  # def initialize
+  #   @collective_punishment = collective_punishment
+  # end
+end
+
+
+Detention.new
